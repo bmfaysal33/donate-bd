6e3884947cdc1acabf18894b375c2aa42352634c
BM Faysal <bmfaysal33@gmail.com>
2025-07-10 15:25:41 +0600

    declared common functions and added selectors

---
+++ Js/library.js
@@ -0,0 +1,32 @@
+
+        // Get Input Values 
+
+        function inputValueById(id){
+            const inputs = document.getElementById(id).value;
+            const inputsNumber = parseFloat(inputs);
+            return inputs;
+
+        }
+        
+
+        // Get text values
+
+        function textValueById(id){
+            const textValue = document.getElementById(id).innerText;
+            const textNumber = parseFloat(textValue);
+            return textNumber;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
