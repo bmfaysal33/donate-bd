99663a754b1e66e59a4f7c94ede4f4dea0999780
BM Faysal <bmfaysal33@gmail.com>
2025-07-10 17:28:11 +0600

    now main balance will be deducted after donation to cards

--- Js/main-scripts.js
+++ Js/main-scripts.js
@@ -1,23 +1,88 @@
 
 
 
-                // Noakhali Section Done 
+// Noakhali Section Done 
 
-           document.getElementById('btn-noakhali').addEventListener('click', function(){
+document.getElementById('btn-noakhali').addEventListener('click', function (event) {
 
-                const noakhaliInputAmount = inputValueById('input-noakhali');
+    event.preventDefault();
 
-                const noakhaliBalance = textValueById('noakhali-balance');
+    const noakhaliInputAmount = inputValueById('input-noakhali');
 
-                const noakhaliNewBalance = noakhaliBalance + noakhaliInputAmount ; 
+    const noakhaliBalance = textValueById('noakhali-balance');
 
-                document.getElementById('noakhali-balance').innerText = noakhaliNewBalance;
+    const noakhaliNewBalance = noakhaliBalance + noakhaliInputAmount;
+
+        // Main Balance Changing 
+
+    const mainBalance = textValueById('main-balance');
+    const newMainBalance = mainBalance - noakhaliInputAmount;
+    document.getElementById('main-balance').innerText = newMainBalance;
+
+    //   Show Noakhali New Balance 
+    document.getElementById('noakhali-balance').innerText = noakhaliNewBalance;
 
 
-            })
+})
 
 
-            
+// Feni Section 
+
+document.getElementById('btn-feni').addEventListener('click', function (event) {
+
+    event.preventDefault();
+
+    const feniInputAmount = inputValueById('input-feni');
+
+    const feniBalance = textValueById('feni-balance');
+
+    const feniNewBalance = feniBalance + feniInputAmount;
+
+
+    
+        // Main Balance Changing 
+
+    const mainBalance = textValueById('main-balance');
+    const newMainBalance = mainBalance - feniInputAmount;
+    document.getElementById('main-balance').innerText = newMainBalance;
+
+        //   Show Feni New Balance 
+
+    document.getElementById('feni-balance').innerText = feniNewBalance;
+
+
+})
+
+
+// Quota Movement section 
+
+
+document.getElementById('btn-quota').addEventListener('click', function (event) {
+
+    event.preventDefault();
+
+    const quotaInputAmount = inputValueById('input-quota');
+
+    const quotaBalance = textValueById('quota-balance');
+
+    const quotaNewBalance = quotaBalance + quotaInputAmount;
+
+    
+        // Main Balance Changing 
+
+    const mainBalance = textValueById('main-balance');
+    const newMainBalance = mainBalance - quotaInputAmount;
+    document.getElementById('main-balance').innerText = newMainBalance;
+
+        //   Show Quota New Balance 
+
+    document.getElementById('quota-balance').innerText = quotaNewBalance;
+
+
+})
+
+
+
 
 
 
